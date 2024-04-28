b44f7ab36cf410f5ac8c44a1330afbe7157ffd22
Chaitanya-Kaypure <167700000+Chaitanya-Kaypure@users.noreply.github.com>
2024-04-28 13:58:43 +0530

    Add files via upload

---
+++ ope_overloading.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+class complex
+{
+int a,b;
+public:
+void getvalue();
+void operator -();
+void operator ++();
+void operator ++(int);
+complex operator+(complex ob);
+complex operator-(complex ob);
+
+void display();
+};
+void complex:: getvalue()
+{
+cout<<"Enter the value of coplex number a,b";
+cin>>a>>b;
+}
+void complex:: operator -()
+{
+a=-a;
+b=-b;
+}
+void complex :: operator ++()
+{
+++a;
+++b;
+}
+void complex :: operator ++(int){
+a++;
+b++;
+}
+complex complex :: operator +(complex ob)
+{
+complex t;
+t,a=a+ob.a;
+t.b=b+ob.b;
+return (t);
+}
+complex complex :: operator -(complex ob)
+{
+complex t;
+t,a=a-ob.a;
+t.b=b-ob.b;
+return (t);
+}
+void complex ::display()
+{
+cout<<a<<"+"<<b<<"i"<<"\n";
+}
+
+
+int main()
+{
+complex obj1,obj2,result,result1;
+
+obj1.getvalue();
+obj2.getvalue();
+cout<<"input value:\n";
+obj1.display();
+obj2.display();
+cout<"result";
+result.display();
+result1.display();
+++obj1;
+obj2++;
+cout<<"after increment :\n";
+obj1.display();
+obj2.display();
+-obj1;
+-obj2;
+cout<<"\n after negatin:";
+obj1.display();
+obj2.display();
+return 0;
+}
+
+
